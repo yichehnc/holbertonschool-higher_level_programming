0f2d489899c92c96b18b09077efa8abdf16a54e3
yichehnc <yichenc2017@gmail.com>
2024-01-25 00:03:42 +1100

    feat: Write a function that prints all integers of a list

---
+++ python-data_structures/0-print_list_integer.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def print_list_integer(my_list=[]):
+    # prints everything in the list
+    i = 0
+    while i < len(my_list):
+        print("{:d}".format(my_list[i]))
+        i = i + 1
