593cc7fae547110829103a57b2e147a4974bde7c
yichehnc <yichenc2017@gmail.com>
2024-01-25 13:31:23 +1100

    feat: Write a function that replaces an element in a list at a specific position without modifying the original list

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -2,7 +2,7 @@
 def new_in_list(my_list, idx, element):
     if idx < 0:
         return my_list
-    elif idx > len(my_list):
+    elif idx > len(my_list)-1:
         return my_list
     else:
         # make a copy to not change the original list
