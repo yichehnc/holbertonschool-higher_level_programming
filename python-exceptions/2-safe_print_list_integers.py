662527da7399242e92c7420d454f2ad26ca6e119
yichehnc <yichenc2017@gmail.com>
2024-01-31 00:59:50 +1100

    feat: Write a function that prints the first x elements and only integers of a list

---
+++ python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    count = 0
+    try:
+        for i in range(x):
+            if type(my_list[i]) == int:
+                print("{:d}".format(my_list[i]), end="")
+                count = count + 1
+    except (TypeError, ValueError):
+        print()
+        return count
+    else:
+        print()
+        return count
