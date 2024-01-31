5d758e7efb9cc05534fec934e0454fcea63c6d72
yichehnc <yichenc2017@gmail.com>
2024-01-31 13:44:36 +1100

    feat: Write a function that divides element by element 2 lists

---
+++ python-exceptions/4-list_division.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+def list_division(my_list_1, my_list_2, list_length):
+    div_list = []
+    for i in range(list_length):
+        try:
+            quotient = my_list_1[i] / my_list_2[i]
+        except ZeroDivisionError:
+            quotient = 0
+            print("division by 0")
+        except IndexError:
+            quotient = 0
+            print("out of range")
+        except TypeError:
+            quotient = 0
+            print("wrong type")
+        finally:
+            div_list.append(quotient)
+    return div_list
