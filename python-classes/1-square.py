70daa63f5c3c8073c44155cda5ab81453b5241de
yichehnc <yichenc2017@gmail.com>
2024-02-01 13:58:20 +1100

    feat: Write a class Square contains a private instance attribute size

---
+++ python-classes/1-square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""A class Square that defines a square"""
+
+
+class Square:
+    """Contains a private instance atrribute size"""
+    def __init__(self, size):
+        self.__size = size
