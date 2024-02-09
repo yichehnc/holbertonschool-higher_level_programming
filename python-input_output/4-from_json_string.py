619d38d70c7e191b7dbd8d16e52c99529cbd1e30
yichehnc <yichenc2017@gmail.com>
2024-02-10 08:20:39 +1100

    feat:a function that returns an object (Python data structure) represented by a JSON string

---
+++ python-input_output/4-from_json_string.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Module for function from_json_strint"""
+import json
+
+
+def from_json_string(my_str):
+    """
+    Convents a JSON string to a Python object 
+
+    Args:
+        my_str (str): the string to convert to JSON
+    
+    Returns:
+        obj: a python object
+    """
+    return json.loads(my_str)
