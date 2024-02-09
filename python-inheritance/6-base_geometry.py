2bf2f43d39805523461a9b23aaeea73178c63eb5
yichehnc <yichenc2017@gmail.com>
2024-02-09 12:11:56 +1100

    feat: a BaseGeometry raises anexception with message area() is not implemented

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Module for class BaseGeomtry"""
+
+
+class BaseGeometry:
+    """class BaseGeometry"""
+
+    def area(self):
+        """calculates area"""
+        raise Exception("area() is not implemented")
