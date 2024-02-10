0b91ea09dad7a8d43975263ed2aa8d9fef597dcd
yichehnc <yichenc2017@gmail.com>
2024-02-11 09:50:11 +1100

    style: fix trailing white space

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -3,7 +3,7 @@
 
 
 def class_to_json(obj):
-	"""
-	returns the dictionary description with simple data structure
-	"""
-	return obj.__dict__
+    """
+    returns the dictionary description with simple data structure
+    """
+    return obj.__dict__
