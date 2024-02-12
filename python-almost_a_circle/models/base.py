ea4ee76f8cee73449196e6ec0477a66e9708ad5c
yichehnc <yichenc2017@gmail.com>
2024-02-13 10:30:11 +1100

    feat: write the first class Base

---
+++ python-almost_a_circle/models/base.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+Module - base.py
+"""
+
+
+class Base():
+    """
+    class Base for Almost a Circle project
+    """
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """
+        class Base constructor
+        """
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects = Base.__nb_objects + 1
+            self.id = Base.__nb_objects
+            
