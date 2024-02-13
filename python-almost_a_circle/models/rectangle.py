0be17f33285481c5dbbad2631ca41025c2f813f5
yichehnc <yichenc2017@gmail.com>
2024-02-13 11:22:37 +1100

    style: fix trailing white space

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -25,7 +25,7 @@ class Rectangle(Base):
         Retrieves itself
         """
         return self.__width
-    
+
     @width.setter
     def width(self, width):
         """
@@ -39,7 +39,7 @@ class Rectangle(Base):
         Retrieves itself
         """
         return self.__height
-    
+
     @height.setter
     def height(self, height):
         """
