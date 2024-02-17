eda4aff81a3a16456020cdde385b77afb7696bf0
yichehnc <yichenc2017@gmail.com>
2024-02-18 08:23:38 +1100

    fix: reorder the integers in unittest for 6-max_integer.py

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -1,22 +1,20 @@
 #!/usr/bin/python
-"""
-Unit tests for '6-max_integer'
-"""
+"""Unit tests for `max_integer`"""
 import unittest
 max_integer = __import__("6-max_integer").max_integer
 
 class TestMaxInteger(unittest.TestCase):
     def test_max_at_end(self):
-        self.assertEqual(max_integer([1, 2, 3, 4, 5], 5))
+        self.assertEqual(max_integer([1, 2, 3, 4, 5]), 5)
 
     def test_max_at_start(self):
-        self.assertEqual(max_integer([5, 4, 3, 2, 1], 5))
+        self.assertEqual(max_integer([5, 4, 3, 2, 1]), 5)
 
     def test_max_in_middle(self):
-        self.assertEqual(max_integer([3, 4, 5, 2, 1], 5))
+        self.assertEqual(max_integer([1, 2, 5, 4, 3]), 5)
 
     def test_all_negative_list(self):
-        self.assertEqual(max_integer([-3, -4, -5, -2, -1], -1))
+        self.assertEqual(max_integer([-1, -2, -3, -4, -5]), -1)
 
     def test_list_with_one_value(self):
         self.assertEqual(max_integer([10]), 10)
@@ -24,5 +22,5 @@ class TestMaxInteger(unittest.TestCase):
     def test_empty_list(self):
         self.assertEqual(max_integer([]), None)
 
-    if __name__ == "__main__":
-        unittest.main()
+if __name__ == "__main__":
+    unittest.main()
