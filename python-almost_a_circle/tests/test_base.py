a22f4c38eb4fcbbce0bb9fda3339e662e3919292
yichehnc <yichenc2017@gmail.com>
2024-02-17 11:17:03 +1100

    feat: add unittest for checking correct json representation with empty and non empty list and none argument

--- python-almost_a_circle/tests/test_base.py
+++ python-almost_a_circle/tests/test_base.py
@@ -25,3 +25,34 @@ class TestBase(unittest.TestCase):
         self.assertEqual(base_1.id + 1, base_2.id)
         base_3 = Base(2)
         self.assertEqual(base_3.id, 2)
+
+    def test_to_json_string_none(self):
+        """
+        Test to_json_string with None as argument
+        """
+        str = Base.to_json_string(None)
+        self.assertEqual(str, "[]")
+
+    def test_to_json_string_empty_list(self):
+        """
+        Test to_json_string with empty list argument
+        """
+        str = Base.to_json_string([])
+        self.assertEqual(str, "[]")
+
+    def test_to_json_string_non_empty_list(self):
+        """
+        Test to_json_string with non empty list argument
+        """
+        str = Base.to_json_string([{"id": 10}])
+        self.assertEqual(str, "[{\"id\": 10}]")
+
+    def test_from_json_string_empty_list(self):
+        """Testing from_json_string with empty list string"""
+        lst = Base.from_json_string("[]")
+        self.assertEqual(lst, [])
+
+    def test_from_json_string_list_with_content(self):
+        """Testing from_json_string with list with content"""
+        lst = Base.from_json_string("[{\"id\": 10}]")
+        self.assertEqual(lst, [{"id": 10}])
