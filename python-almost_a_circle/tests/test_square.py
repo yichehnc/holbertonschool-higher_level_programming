278a093eed7e0e58a699707e9dcda8aa663c235a
yichehnc <yichenc2017@gmail.com>
2024-02-17 11:49:47 +1100

    feat: add unittest for missing modifying zero arguments, negative or string integers

--- python-almost_a_circle/tests/test_square.py
+++ python-almost_a_circle/tests/test_square.py
@@ -4,7 +4,7 @@ Unittest for Square module
 """
 import unittest
 from models.base import Base
-from models.rectangle import Rectangle
+from models.square import square
 from models.square import Square
 from io import StringIO
 import io
@@ -25,3 +25,54 @@ class TestSquare(unittest.TestCase):
         self.assertEqual(square_1.x, 2)
         self.assertEqual(square_1.y, 3)
         self.assertEqual(square_1.id, 4)
+        
+    def test_square_missing_arguments(self):
+            """
+            Test for missing arguments
+            """
+            square_1 = Square(1, 2)
+            self.assertEqual(square_1.size, 1)
+            self.assertEqual(square_1.x, 2)
+            self.assertEqual(square_1.y, 0)
+
+    def test_square_adding_arguments(self):
+            """
+            Test for modifying and adding arguments after initialization
+            """
+            square_1 = Square(1, 2)
+            self.assertEqual(square_1.size, 1)
+            self.assertEqual(square_1.x, 2)
+            self.assertEqual(square_1.y, 0)
+            square_1.y = 3
+            square_1.id = 4
+            self.assertEqual(square_1.y, 3)
+            self.assertEqual(square_1.id, 4)
+
+    def test_square_negative(self):
+            """
+            Test for negative integer arguments
+            """
+            with self.assertRaises(ValueError):
+                square_1 = Square(-1, 2, 3)
+            with self.assertRaises(ValueError):
+                square_1 = Square(1, -2, 3)
+            with self.assertRaises(ValueError):
+                square_1 = Square(1, 2, -3)
+
+    def test_square_string(self):
+        """
+        Test for string arguments
+        """
+        with self.assertRaises(TypeError):
+            square_1 = Square("1", 2, 3)
+        with self.assertRaises(TypeError):
+            square_1 = Square(1, "2", 3)
+        with self.assertRaises(TypeError):
+            square_1 = Square(1, 2, "3")
+
+    def test_square_zero(self):
+            """
+            Test for argument is 0
+            """
+            with self.assertRaises(ValueError):
+                square_1 = Square(0, 2)
