c4a343d98daaf13a3ddb7ea490f6cf3677497bc6
yichehnc <yichenc2017@gmail.com>
2024-03-17 08:55:53 +1100

    feat: create the database hbtn_0d_usa and talbe cities

---
+++ SQL_more_queries/7-cities.sql
@@ -0,0 +1,11 @@
+-- Creating the database hbtn_0d_usa and the table cities
+-- in the MySQL server.
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+DROP TABLE IF EXISTS hbtn_0d_usa.cities;
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (
+    id INT UNIQUE NOT NULL AUTO_INCREMENT,
+    state_id INT NOT NULL,
+    name VARCHAR(256) NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (state_id) REFERENCES states (id)
+);
