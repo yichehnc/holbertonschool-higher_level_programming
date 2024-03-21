3c7a8234fdc93f8797ddead357d7cae85028fddf
yichehnc <yichenc2017@gmail.com>
2024-03-21 13:15:20 +1100

    feat: lists all shows contained in the database hbtn_0d_tvshows

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -1,5 +1,5 @@
 -- Lists all shows contained in hbtn_0e_14_usa that have at least one genre linked
 SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows
-LEFT JOIN tv_show_genres ON tv_show.id = tv_show_genres.show_id
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
 WHERE tv_show_genres.show_id IS NOT NULL
 ORDER BY tv_shows.title, tv_show_genres.genre_id ASC;
