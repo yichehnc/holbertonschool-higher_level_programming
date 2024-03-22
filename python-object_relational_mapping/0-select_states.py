4c7517dd79c56a1df46b33c1f0b8f2d008194162
yichehnc <yichenc2017@gmail.com>
2024-03-23 07:57:42 +1100

    style: format to pycodestyle

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -6,7 +6,7 @@ import MySQLdb
 import sys
 
 if __name__ == "__main__":
-    database = MySQLdb.connect(host="localhost", user=sys.argv[1], 
+    database = MySQLdb.connect(host="localhost", user=sys.argv[1],
                                passwd=sys.argv[2], db=sys.argv[3])
     cursor = database.cursor()
     cursor.execute("SELECT * FROM states ORDER BY id;")
