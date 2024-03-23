177c7ff36a6ba8ba113d1612359ee6553375b06d
yichehnc <yichenc2017@gmail.com>
2024-03-24 09:43:20 +1100

    feat: lists all cities from the database

---
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+Scripts that lists all cities from the database hbtn_0e_4_usa
+"""
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    database = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                               passwd=sys.argv[2], db=sys.argv[3])
+
+    cursor = database.cursor()
+    cursor.execute("SELECT cities.id, cities.name, states.name FROM cities \
+                   LEFT JOIN states ON cities.state_id = states.id ORDER BY \
+                   cities.id ASC;")
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
+        cursor.close()
+        database.close()
